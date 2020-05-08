24b024b44f46ecf21b6a107b2528b827a420ec09
lucasferreiradaluz <lucas.luz.lucas@gmail.com>
2020-05-08 17:22:23 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# multiplicationtable_js
+ project made in JavaScript, HTML and CSS. It consists of showing multiplication tables of any number.
---
+++ script.js
@@ -0,0 +1,20 @@
+function tabuada(){
+    let num = document.getElementById("txtn");
+    let tab = document.getElementById("seltab");
+
+    if(num.length == 0){
+        window.alert("[ERROR] Digite um número")
+    }else{
+        let n = Number(num.value);
+        for(let c = 1; c <= 10; c++){
+            let item = document.createElement('option');
+            item.text = `${n} x ${c} = ${n*c}`;
+            tab.appendChild(item);
+        }
+    }
+
+}
+
+function refresh(){
+    window.location.reload()
+}
